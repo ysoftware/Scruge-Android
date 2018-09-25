7cddb91c61fe95ff64a2eb5670d38cef1c4f2fcc
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-09-25 12:51:27 +0300

    Initial commit

---
+++ app/src/main/java/com/scruge/scruge/view/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.scruge.scruge.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.scruge.scruge.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
