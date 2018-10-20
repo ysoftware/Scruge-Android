365c87324a628f1dca3158d3c3279404572edd8f
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-10-20 12:12:48 +0300

    migration to androidX

--- app/src/main/java/com/scruge/scruge/view/activity/MainActivity.kt
+++ app/src/main/java/com/scruge/scruge/view/activity/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.scruge.scruge.view.activity
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.scruge.scruge.R
 
