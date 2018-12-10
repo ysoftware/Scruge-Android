88b23165be48607942d06bb62e80f26d441a36ac
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-12-10 18:08:51 +0300

    top comment cell

--- app/src/main/java/com/scruge/scruge/view/cells/Comment.kt
+++ app/src/main/java/com/scruge/scruge/view/cells/Comment.kt
@@ -2,17 +2,43 @@ package com.scruge.scruge.view.cells
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.scruge.scruge.dependencies.view.setImage
 import com.scruge.scruge.viewmodel.comment.CommentAVM
+import kotlinx.android.synthetic.main.cell_comment_top.view.*
 
 class TopCommentCell(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun setup(vm:CommentAVM?, allCommentsCount:Int):TopCommentCell {
+        if (vm != null && !vm.isEmpty) {
+            val comment = vm.item(0)
 
+            itemView.top_comment_name.text = comment.authorName
+            itemView.top_comment_text.text = comment.comment
+            itemView.top_comment_date.text = comment.date
+            itemView.top_comment_likes.text = comment.likes
+
+            comment.authorPhoto?.let {
+                itemView.top_comment_profile_image.setImage(it)
+            } ?: itemView.top_comment_profile_image.setImageDrawable(null)
+
+            itemView.top_comment_see_all.text = if (allCommentsCount == 1) "Add your comment"
+                else "See all $allCommentsCount comments"
+
+            itemView.top_comment_view.visibility = View.VISIBLE
+            itemView.top_comment_no_view.visibility = View.GONE
+        }
+        else {
+            itemView.top_comment_see_all.text = "Add your comment"
+            itemView.top_comment_view.visibility = View.GONE
+            itemView.top_comment_no_view.visibility = View.VISIBLE
+        }
         return this
     }
 
     fun allComments(tap:()->Unit):TopCommentCell {
-
+        itemView.top_comment_see_all_view.setOnClickListener {
+            tap()
+        }
         return this
     }
 }
