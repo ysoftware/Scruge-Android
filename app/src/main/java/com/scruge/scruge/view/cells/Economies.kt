78fb0ffc43eae007f148d22c3b2a065d75018c4c
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-12-10 17:10:50 +0300

    economies cell

--- app/src/main/java/com/scruge/scruge/view/cells/Economies.kt
+++ app/src/main/java/com/scruge/scruge/view/cells/Economies.kt
@@ -3,11 +3,16 @@ package com.scruge.scruge.view.cells
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.scruge.scruge.viewmodel.economies.EconomiesVM
+import kotlinx.android.synthetic.main.cell_economies.view.*
 
 class EconomiesCell(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun setup(vm:EconomiesVM):EconomiesCell {
 
+        itemView.economies_inflation.text = vm.inflationRate
+        itemView.economies_initial_release.text = vm.initialRelease
+        itemView.economies_public_percent.text = vm.publicPercent
+        itemView.economies_supply.text = vm.tokenSupply
         return this
     }
 }
