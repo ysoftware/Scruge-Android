0aec7c0db3959b786ba93b598631ab8da13cd5c6
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-12-11 12:01:49 +0300

    image loading

--- app/src/main/java/com/scruge/scruge/dependencies/view/DownloadImage.kt
+++ app/src/main/java/com/scruge/scruge/dependencies/view/DownloadImage.kt
@@ -3,11 +3,26 @@ package com.scruge.scruge.dependencies.view
 import android.net.Uri
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 fun ImageView.setImage(url: Uri?, hideOnFail:Boolean = true) {
-    Picasso.get().load(url).fit().centerCrop().into(this)
+    Picasso.Builder(context)
+            .listener { _, _, _ ->
+                if (hideOnFail) {
+                    setImageDrawable(null)
+                }
+            }
+            .build()
+            .load(url)
+            .fit()
+            .centerCrop()
+            .into(this)
 }
 
 fun ImageView.setImage(url: String?, hideOnFail:Boolean = true) {
-    Picasso.get().load(url).fit().centerCrop().into(this)
+    try {
+        val uri = Uri.parse(url)
+        setImage(uri, hideOnFail)
+    }
+    catch (ex:Exception) { }
 }
